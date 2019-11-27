a24b98e0dcb550896d50f494f0b8fdc5318cb2cf
neihtfool2 <q.thien.nguyen@outlook.de>
2019-11-27 16:24:20 +0100

    change prot to 8000

--- server.js
+++ server.js
@@ -57,6 +57,6 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
 
-app.listen(3000, function() {
-    console.log("listening on port 3000!");
+app.listen(8000, function() {
+    console.log("listening on port 8000!");
 })
