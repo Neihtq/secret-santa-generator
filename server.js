4344099e59210845d3e1c6e5d4261f50fb18fea4
Thien <q.thien.nguyen@outlook.de>
2019-11-27 17:02:04 +0100

    enable cors

--- server.js
+++ server.js
@@ -37,6 +37,12 @@ function assign(secretSanta) {
 }
 app.use('/', express.static(__dirname + '/'));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*'); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.get('/names', function (req, res) {
     res.json(JSON.stringify({names: names}))
 })
